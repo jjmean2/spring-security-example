53569246af05a09ad6735078666791c6989158d1
Jongwan Lee <jjmean2@gmail.com>
2017-07-25 18:25:21 +0900

    SecurityConfig 변경

--- src/main/java/com/vovios/config/SecurityConfig.java
+++ src/main/java/com/vovios/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vovios.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,21 +12,17 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    @Bean
     protected UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("user").password("password").roles("USER").build());
         return manager;
     }
-    
-   
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+        http.authorizeRequests()
+                .anyRequest().authenticated().and().httpBasic();
     }
 
 }
